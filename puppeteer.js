d35edf60da47d8934d2bc5faf3bce092764db5d6
Armando Silveira <armando.silveira.work@gmail.com>
2020-12-31 18:43:35 -0500

    More refactors adds more command line elements

--- puppeteer.js
+++ puppeteer.js
@@ -12,9 +12,10 @@ const AMAZON_PHOTOS_URL = 'https://www.amazon.com/photos/all';
 const INPUT_DELAY_IN_MILLISECONDS = 50;
 
 const COOKIES_FILENAME = 'cookies.json';
-const CREDENTIALS_FILENAME = 'credentials.json'
+const CREDENTIALS_FILENAME = 'credentials.json';
+const METADATA_FILENAME = 'all-metadata.json';
 
-const CAPTURED_REQS_FILENAME = 'dev-captured-requests.json'
+const CAPTURED_REQS_FILENAME = 'dev-captured-requests.json';
 const SCREENSHOT_FILENAME = 'dev-screenshot.png';
 
 const sleep = (millis) => new Promise((resolve) => setTimeout(resolve, millis));
@@ -49,6 +50,8 @@ const loadCredentials = () => {
 };
 
 const login = async (page, username, password) => {
+  console.log('Logging in...');
+  await page.goto(AMAZON_PHOTOS_URL);
   if (isAtLoginScreen(page)) {
     // login logic
     await page.type('[type=email]', username, { delay: delayTime() });
@@ -166,16 +169,32 @@ const loadHeaders = (cdpRequestDataRaw) => {
   return headers;
 };
 
+const loadMetaDataFile = () => {
+  if (fs.existsSync('./' + METADATA_FILENAME)) {
+    return JSON.parse(fs.readFileSync('./' + METADATA_FILENAME, { encoding: 'utf8' }));
+  } else {
+    console.log('local metadata file not found');
+  }
+}
+
+const listAllAmazonPhotos = (metadata) => {
+  metadata.data.forEach(unknownArray => {
+    unknownArray.forEach(photo => {
+      console.log(photo.name);
+    });
+  });
+  console.log("Number of photos: " + metadata.count);
+}
+
 const getAllFileMetaData = async (headers) => {
-  console.time('metadata');
   let done = false;
   let numberOfFiles;
   let page = 0;
   const data = [];
   do {
-    const { body } = await mimicSearchRequest(page, headers);
-    // fs.writeFileSync('./captured-response-' + page + '.json', body);
-    payload = JSON.parse(body);
+    // const { body } = await mimicSearchRequest(page, headers);
+    // payload = JSON.parse(body);
+    const payload = await mimicSearchRequest(page, headers);
     data.push(payload.data);
     numberOfFiles = payload.count;
     page += 1;
@@ -183,10 +202,10 @@ const getAllFileMetaData = async (headers) => {
       done = true;
     }
   } while (!done);
-  fs.writeFileSync('./all-metadata.json', JSON.stringify({ data, count: numberOfFiles }, null, 4));
+  fs.writeFileSync('./' + METADATA_FILENAME, JSON.stringify({ data, count: numberOfFiles }, null, 4));
   console.log(console.timeEnd('metadata'));
   // Old code snippet that would scroll down to attempt to trigger all the requests
-  // hella slow... if revisiting this implementation look into using the newly added mouse.wheel function
+  // way too slow... if revisiting this implementation look into using the newly added mouse.wheel function
   // let oC = 0;
   // while (stillRecievingFiles.well(counter)) {
   //   if (oC != counter) {
@@ -217,22 +236,27 @@ const getAllFileMetaData = async (headers) => {
   // };
 };
 
-const mimicSearchRequest = (page, headers) => {
-  let url = 'https://www.amazon.com/drive/v1/search?asset=NONE&filters=type%3A(PHOTOS+OR+VIDEOS)&limit=1&searchContext=customer&sort=%5B%27contentProperties.contentDate+DESC%27%5D&tempLink=false&resourceVersion=V2&ContentType=JSON&_=';
-  url += Date.now().toString();
-  url = url.replace('&limit=1', '&limit=200');
-  if (page > 0) {
-    url = url.replace('&tempLink=false', `&tempLink=false&offset=${200 * page}`);
+const mimicSearchRequest = async (page, headers) => {
+  resp = {};
+  try {
+    let url = 'https://www.amazon.com/drive/v1/search?asset=NONE&filters=type%3A(PHOTOS+OR+VIDEOS)&limit=1&searchContext=customer&sort=%5B%27contentProperties.contentDate+DESC%27%5D&tempLink=false&resourceVersion=V2&ContentType=JSON&_=';
+    url += Date.now().toString();
+    url = url.replace('&limit=1', '&limit=200');
+    if (page > 0) {
+      url = url.replace('&tempLink=false', `&tempLink=false&offset=${200 * page}`);
+    }
+    resp = await got.get(url, {
+      headers
+    }).json();
+  } catch (error) {
+    console.log(error.response);
+    resp = error;
   }
-  return got.get(url, {
-    headers,
-    responseType: 'json',
-    resolveBodyOnly: true,
-  });
+  fs.writeFileSync('./dev-captured-response-' + page + '.json', JSON.stringify(resp));
+  return resp;
 };
 
 const mimicDownloadRequest = (headers) => {};
-
 // Credit: https://stackoverflow.com/questions/47078655/missing-request-headers-in-puppeteer/62232903#62232903
 // Returns map of request ID to raw CDP request data. This will be populated as requests are made.
 // NOTE: This is not saving request/response payloads - Don't know why | may be trapped behind a function call...
@@ -258,16 +282,9 @@ const setupPage = async (browser) => {
   const pages = await browser.pages();
   const page = pages[0];
 
-  try {
-    const cookies = JSON.parse(fs.readFileSync('./' + COOKIES_FILENAME));
-    for (const cookie of cookies) { // setting the cookies individually seems to make puppeteer happy
-      page.setCookie(cookie);
-    }
-  } catch (error) {
-    console.log(error);
-  }
+  loadCookiesFromFileToPage(page);
 
-  // Setup request interception TODO: since this is only useful for dev purposes, don't call during prod
+  // Setup request interception
   const cdpRequestDataRaw = await setupLoggingOfAllNetworkData(page);
 
   // Setup response interception
@@ -280,57 +297,91 @@ const setupPage = async (browser) => {
   // });
 
   // Initiate login
-  console.log('Logging in...');
-  await page.goto(AMAZON_PHOTOS_URL);
   await login(page, username, password);
 
-  // Save captured request data... for science
+  // Save captured request data... for debugging
   fs.writeFileSync('./' + CAPTURED_REQS_FILENAME, JSON.stringify(cdpRequestDataRaw, null, 4));
 
   const headers = loadHeaders(cdpRequestDataRaw);
-  console.log(JSON.stringify(headers));
-  await getAllFileMetaData(headers);
+  // console.log(JSON.stringify(headers));
 
   const gimmeTheCookies = await page.cookies();
-  console.log(JSON.stringify(gimmeTheCookies));
+  // console.log(JSON.stringify(gimmeTheCookies));
   fs.writeFileSync('./' + COOKIES_FILENAME, JSON.stringify(gimmeTheCookies, null, 4));
+
+  return {
+    page,
+    headers
+  }
 };
+
+const loadCookiesFromFileToPage = async (page) => {
+  if (fs.existsSync('./' + COOKIES_FILENAME)) {
+    try {
+      const cookies = JSON.parse(fs.readFileSync('./' + COOKIES_FILENAME));
+      for (const cookie of cookies) { // setting the cookies individually seems to make puppeteer happy
+        page.setCookie(cookie);
+      }
+    } catch (error) {
+      console.log(error);
+      exit(1);
+    }
+  } else {
+    console.log('previous cookies not found...');
+  }
+};
+
+let commandArguments = {
+  'list': 'lists the files that have been uploaded',
+  'reset': 'deletes the cookies and metadata saved from previous launches',
+  'upload': 'upload [filename].png to upload file (must be in project directory). To upload a file split between multiple images e.g. [filename]-3.png just omit the number and the application will auto upload all the files',
+  'download': 'download [filename] to download a file that is in the project directory',
+  'help': 'shows all commands'
+};
+
+let page = null;
+let headers = null;
 
 const main = async () => {
   const myArgs = process.argv.slice(2);
 
   loadCredentials();
-
-  // TODO: If the cookies don't exist or have expired then launch normally, if not headless mode should work fine
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-
-  const page = await setupPage(browser);
+  const metadata = loadMetaDataFile();
   
   switch (myArgs[0]) {
+    case 'init':
+      await getAllFileMetaData(headers);
+      break;
     case 'list':
-      // TODO:
+      await listAllAmazonPhotos(metadata);
+      break;
+    case 'login':
+      // TODO: If the cookies don't exist or have expired then launch normally, if not headless mode should work fine
+      const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: null,
+      });
+      const {page, headers} = await setupPage(browser);
+      await browser.close();
+      break;
+    case 'reset':
+      console.log('to be implemented');
       break;
     case 'upload':
-      await uploadPage(page, './' + myArgs[1]); // TODO: add support for relative filepaths
-      await sleep(5000); // manual sleep until I figure out how to continue once the page is done loading
+      console.log('to be implemented');
+      // await uploadPage(page, './' + myArgs[1]); // TODO: add support for relative filepaths
+      // await sleep(5000); // manual sleep until I figure out how to continue once the page is done loading
       break;
     case 'download':
       // download the provided file
+      console.log('to be implemented');
       break;
     case 'help':
-      console.log(`Here are the commands available:
-        list - to list the files that have been uploaded
-        upload [filename].png to upload a file image. To upload a file split between multiple images e.g. [filename]-3.png do not include the -3 and the app will auto upload all of the files
-        download [filename] to download a file image. To download a file that is split between multiple images e.g. [filename]-3 do not include the -3 and the app will auto download all of the files`)
+      console.log(`Here are the commands available:` + JSON.stringify(commandArguments));
       break;
     default:
       console.log('Error while parsing commands use help for usage examples')
   }
-
-  await browser.close();
 }
 
 // (async () => {
